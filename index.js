2f91639c806518e3d66443a4b3e5fe58a9dc6154
VulpineReynard <milare8@gmail.com>
2019-09-16 13:45:42 -0400

    added authorization

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
+  let { username, password } = req.body;
 
-  Users.add(user)
+  const hash = bcrypt.hashSync(password, 8);
+
+  Users.add({ username, password: hash })
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -34,10 +36,10 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+      if (!user || !bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ error: 'Incorrect credentials' });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(201).json({ message: `Welcome ${username}.` })
       }
     })
     .catch(error => {
@@ -45,20 +47,46 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', validate, (req, res) => {
   Users.find()
     .then(users => {
-      res.json(users);
+      console.log(users)
+      res.status(200).json(users);
     })
     .catch(err => res.send(err));
 });
-server.get('/hash', (req, res) => {
-  const name = req.query.name;
-
-  // hash the name
-  const hash = bcrypt.hashSync(name, 14);
-  res.send(`The hash for ${name} is ${hash}.`)
-})
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+function validate(req, res, next) {
+  // we'll read the username and password from headers
+  // the client is responsible for setting those headers
+  const { username, password } = req.headers;
+
+  // no point on querying the database if the headers are not present
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Unexpected error' });
+      });
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
+}
+
+/* 
+write middleware that will check for the username and password
+and let the request continue to /api/users if credentials are good
+return a 401 if th credentials are invalid
+
+use the middleware to restrict acces to GET /api/users
+*/
